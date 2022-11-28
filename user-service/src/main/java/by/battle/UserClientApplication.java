f2a2466d84c39cfeb7504bbd621cfee0165e1a7f
MaksimShumkov <MaksimShumkov@coherentsolutions.com>
2022-11-28 14:20:36 +0300

    Refactored structure of project

--- user-service/src/main/java/by/battle/UserClientApplication.java
+++ user-service/src/main/java/by/battle/UserClientApplication.java
@@ -1,10 +1,9 @@
 package by.battle;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class UserClientApplication {
 
