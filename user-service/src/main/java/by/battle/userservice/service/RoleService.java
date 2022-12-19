5fd6dd4b4b62976aacbef5dac4afb0756a1f311d
MaksimShumkov <MaksimShumkov@coherentsolutions.com>
2022-12-19 15:41:56 +0300

    user-service adding liquibase and fixed Role

--- user-service/src/main/java/by/battle/userservice/service/RoleService.java
+++ user-service/src/main/java/by/battle/userservice/service/RoleService.java
@@ -1,11 +1,12 @@
 package by.battle.userservice.service;
 
 import by.battle.userservice.entity.Role;
+import by.battle.userservice.entity.RoleName;
 
 import java.util.List;
 
 public interface RoleService {
-    Role findByName(String name);
+    Role findByName(RoleName name);
 
     List<Role> findAll();
 }
