e0e2510a536b7099cb0c2acc034f6b88d9ba564e
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2023-02-25 20:53:11 +0300

    MaximShumkov_Infrastructure_module - add module and fix logic (#7)
    * MaximShumkov_Infrastructure_module - add module and fix logic
    
    * MaximShumkov_Infrastructure_module - add module and fix logic2

---
+++ applications/game-service/src/main/java/by/battle/gameservice/GameServiceApplication.java
@@ -0,0 +1,18 @@
+package by.battle.gameservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableEurekaClient
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = "by.battle")
+public class GameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class, args);
+    }
+}
---
+++ applications/game-service/src/main/java/by/battle/gameservice/service/impl/GameServiceImpl.java
@@ -0,0 +1,164 @@
+package by.battle.gameservice.service.impl;
+
+
+import by.battle.errorhandler.exception.FieldNotFreeException;
+import by.battle.errorhandler.exception.NotUserTurnException;
+import by.battle.errorhandler.exception.ObjectAlreadyExistsException;
+import by.battle.errorhandler.exception.ResourceNotFoundException;
+import by.battle.gameservice.entity.*;
+import by.battle.gameservice.repository.GameRepository;
+import by.battle.gameservice.service.GameService;
+import by.battle.gameservice.service.MoveService;
+import by.battle.gameservice.service.ResultService;
+import by.battle.gameservice.service.UserService;
+import by.battle.gameservice.util.FakeSecurityHolder;
+import by.battle.gameservice.util.WinnerCombinationChecker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class GameServiceImpl implements GameService {
+
+    private final GameRepository gameRepository;
+    private final UserService userService;
+    private final ResultService resultService;
+    private final MoveService moveService;
+    private final FakeSecurityHolder fakeSecurityHolder;
+    private final WinnerCombinationChecker winnerCombinationChecker;
+
+    @Override
+    public Game create(Game game) {
+        isFiguresAreDifferent(game);
+        game.setName(String.format("Battle %s VS %s", game.getUsers().get(0).getName(), game.getUsers().get(1).getName()));
+        game.setStatus(GameStatus.STARTED);
+        game.setCells(generateCellsFromSize(game));
+        game.setUsers(getUsersFromDbIfExist(game.getUsers()));
+        setResultInProgress(game);
+        game.getUsers().forEach(userService::save);
+        game.setPlayerFigures(createPlayerFigures(game));
+        return gameRepository.save(game);
+    }
+
+    @Override
+    public Optional<Game> getById(String id) {
+        return gameRepository.findById(id);
+    }
+
+    @Override
+    public Game play(Move move) {
+        String userId = fakeSecurityHolder.getTestUserOneId();
+        move.setUser(userService.findById(userId));
+        Game game = findGame(move);
+        isGameHasNotStatusFinished(game);
+        isUserTurn(game, move);
+        isFieldNotFree(move, game);
+        moveService.save(move);
+        game.setStatus(GameStatus.WAITING_FOR_OPPONENT);
+        return checkWinner(game) ? getFinishedGame(game, move) : saveGame(game);
+    }
+
+    private List<PlayerFigure> createPlayerFigures(Game game) {
+        return game.getPlayerFigures().stream()
+                .peek(it -> it.setGame(game))
+                .map(it -> it.setUser(userService.findByName(it.getUserName())
+                        .orElseGet(it::getUser)))
+                .collect(Collectors.toList());
+    }
+
+    private List<User> getUsersFromDbIfExist(List<User> users) {
+        return users.stream().map(it -> userService
+                .findByName(it.getName()).orElseGet(() -> it)).collect(Collectors.toList());
+    }
+
+    private List<Cell> generateCellsFromSize(Game game) {
+        int size = game.getSize();
+        List<Cell> cells = new ArrayList<>();
+        for (int i = size; i > 0; i--) {
+            for (int j = 1; j <= size; j++) {
+                cells.add(new Cell().setHorizontalIndex(i).setVerticalIndex(j).setGame(game));
+            }
+        }
+        return cells;
+    }
+
+    private Game saveGame(Game game) {
+        return gameRepository.save(game);
+    }
+
+    private Game getFinishedGame(Game game, Move move) {
+        game.setStatus(GameStatus.FINISHED);
+        return gameRepository.save(saveResults(game, move));
+    }
+
+    private Game findGame(Move move) {
+        return gameRepository.findById(move.getGame().getId())
+                .orElseThrow(() -> new ResourceNotFoundException(move.getGame().getName()));
+    }
+
+    private void isGameHasNotStatusFinished(Game game) {
+        if (game.getStatus().equals(GameStatus.FINISHED)) {
+            throw new ResourceNotFoundException(game.getName() + " finished");
+        }
+    }
+
+    private void isFieldNotFree(Move move, Game game) {
+        if (moveService.findAllByGameId(game.getId()).stream()
+                .anyMatch(it -> it.getCell().equals(move.getCell()))) {
+            throw new FieldNotFreeException(move.getCell().toString());
+        }
+    }
+
+    private boolean checkWinner(Game game) {
+        return winnerCombinationChecker
+                .checkWin(game.getMoves());
+    }
+
+    private Game saveResults(Game game, Move move) {
+        saveResultWin(game, move);
+        saveResultLoose(game, move);
+        return game;
+    }
+
+    private void saveResultLoose(Game game, Move move) {
+        User userLoose = game.getUsers().stream()
+                .filter(it -> !it.equals(move.getUser())).findFirst().get();
+        ResultUser resultUser = resultService.findByUserAndGame(userLoose, game).get().setResult(Result.LOOSE);
+        resultService.save(resultUser);
+    }
+
+    private void saveResultWin(Game game, Move move) {
+        ResultUser resultUser = resultService.findByUserAndGame(move.getUser(), game).get().setResult(Result.WIN);
+        resultService.save(resultUser);
+    }
+
+    private void setResultInProgress(Game game) {
+        game.setResults(List.of(createResultUser(game, game.getUsers().get(0), Result.IN_PROGRESS),
+                createResultUser(game, game.getUsers().get(1), Result.IN_PROGRESS)));
+    }
+
+    private ResultUser createResultUser(Game game, User user, Result result) {
+        return new ResultUser()
+                .setResult(result)
+                .setUser(user)
+                .setGame(game);
+    }
+
+
+    private void isFiguresAreDifferent(Game game) {
+        if (game.getPlayerFigures().get(0).getFigure().equals(game.getPlayerFigures().get(1).getFigure())) {
+            throw new ObjectAlreadyExistsException(game.getPlayerFigures().get(0).getFigure().toString());
+        }
+    }
+
+    private void isUserTurn(Game game, Move move) {
+        moveService.findAllByGameId(game.getId()).stream()
+                .filter(Objects::nonNull).max(Comparator.comparing(Move::getCreatedAt))
+                .stream().filter(it -> it.getUser().equals(move.getUser())).findAny().ifPresent(s -> {
+                    throw new NotUserTurnException(move.getUser().getId());
+                });
+    }
+}
